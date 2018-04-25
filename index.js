3bf3c83687e05670b3724a3225124348548f1e8c
Peter Ajtai <peter.ajtai@soliddigital.com>
2018-04-25 13:31:55 -0700

    Initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+const pino = require('pino')();
+
+pino.info('starting app');
+app.listen(process.env.PORT);
+console.log(`Listening on port ${process.env.PORT}`);
+
