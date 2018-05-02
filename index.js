8a045f7b4f3ddbc6af1ff297b204017fd5dd10cc
Peter Ajtai <peter.ajtai@soliddigital.com>
2018-05-02 09:52:36 -0700

    404

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ console.log('starting app');
 app.get('/version', (req, res) => {
    res.send({ version });
 });
+app.get('*', (req, res) => {
+   res.send({ message: 'Resource not found' }, 404);
+});
 
 console.log('listening at http://localhost:3333');
 app.listen(3333);
