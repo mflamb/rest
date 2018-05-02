c6bdd7d12c4867970f33089aa3a483478bafee4b
Peter Ajtai <peter.ajtai@soliddigital.com>
2018-05-02 15:13:39 -0700

    Nodemon

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 
 const express = require('express');
 const app = express();
+
 const { version } = require('./package');
 
 console.log('starting app');
 app.get('/version', (req, res) => {
-   res.send({ version });
+    res.send({ version });
 });
 app.get('*', (req, res) => {
-   res.send({ message: 'Resource not found' }, 404);
+    res.status(404).send({ message: 'Resource not found' });
 });
 
 console.log('listening at http://localhost:3333');
