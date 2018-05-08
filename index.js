f821b8bab79190e308195d8659510965f0f2b453
Peter Ajtai <peter.ajtai@soliddigital.com>
2018-05-08 11:38:24 -0700

    Swagger ui

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ server.use(helmet());
 
 
 const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const options = {
     apis: ['./models/**/*.model.js', './api/**/*.controller.js'],
@@ -39,12 +40,13 @@ const options = {
 // Initialize swagger-jsdoc -> returns validated swagger spec in json format
 const swaggerSpec = swaggerJSDoc(options);
 
+server.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 // Serve our json from express
 server.get('/api/api-docs.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
 });
-
 
 const app = {
     connection
